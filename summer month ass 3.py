ef43710ff6bb26d389b571ffa880830bbd2509ae
ROHITH KS <124114171+rohith660@users.noreply.github.com>
2025-07-11 22:02:46 +0530

    Add files via upload

---
+++ summer month ass 3.py
@@ -0,0 +1,11 @@
+#K.S.Rohith -192210070
+Month-input ("Enter the Month: ")
+Date=int(input ("Enter the Date: "))
+if Month = 'March' and Date==20:
+    print ("THE SEASON IS CURRENTLY SUMMER")
+elif Month=='June' and Date==21:
+    print("THE SEASON IS CURRENTLY SPRING")
+elif Month == 'September' and Date==22:
+    print ("THE SEASON IS CURRENTLY FALL") elif Month=='December' and Date==21: print ("THE SEASON IS CURRENTLY WINTER")
+else:
+    print ("NOT PRESENT IN THE CALANDER")
