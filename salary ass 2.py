7386b90f1d37e70d83ed080213d38ce34b2ea188
ROHITH KS <124114171+rohith660@users.noreply.github.com>
2025-07-13 11:31:40 +0530

    Add files via upload

---
+++ salary ass 2.py
@@ -0,0 +1,15 @@
+#K.S.Rohith - 192210070
+grade-input ("Enter the grade A or B: ")
+
+salary-float (input ("Enter the salary: "))
+
+if grade--'A': bonus-0.5* salary
+
+elif grade=='B':
+     bonus=0.1*salary
+elif salary<10000:
+     bonus+-0.2*salary
+
+print ("Bonus is: ",bonus)
+total_salary-salary+bonus
+print("Total salary is: ",total_salary)
---
+++ shs.py
@@ -0,0 +1,23 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/rohit/OneDrive/Desktop/python programming/simple interset.py
+enter the principle amount: 200000
+enter the time period: 3
+enter y or n: y
+enter m or n: m
+72000.0
+
+= RESTART: C:/Users/rohit/OneDrive/Desktop/python programming/simple interset.py
+enter the grade a or b: b
+Traceback (most recent call last):
+  File "C:/Users/rohit/OneDrive/Desktop/python programming/simple interset.py", line 1, in <module>
+    grade=int(input("enter the grade a or b: "))
+ValueError: invalid literal for int() with base 10: 'b'
+>>> 
+= RESTART: C:/Users/rohit/OneDrive/Desktop/python programming/simple interset.py
+enter the grade a or b: b
+Traceback (most recent call last):
+  File "C:/Users/rohit/OneDrive/Desktop/python programming/simple interset.py", line 1, in <module>
+    grade=int(input("enter the grade a or b: "))
+ValueError: invalid literal for int() with base 10: 'b'
